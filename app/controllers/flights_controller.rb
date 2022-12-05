88e9fc86d8c717c1376bfe7d8e783951962308be
fliblip <wickynjy@gmail.com>
2022-12-05 16:08:01 +0300

    Initialized routes

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,2 +1,5 @@
 class FlightsController < ApplicationController
+    def index
+        render json: Flight.all
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  resources :flights
-  resources :users
+  resources :flights, only: [:index]
+  resources :users, only: [:index]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
