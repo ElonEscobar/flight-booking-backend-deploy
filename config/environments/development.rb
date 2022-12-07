4e661dfb0abba0da31461ced1423ec34b4140964
dan muendo <danwalker254254@gmail.com>
2022-12-07 20:13:46 +0000

    updated files

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -17,7 +17,7 @@ Rails.application.configure do
   # Enable server timing
   config.server_timing = true
 
-  config.hosts << "flight-booking-backend-production.up.railway.app" 
+  # config.hosts << "flight-booking-backend-production.up.railway.app" 
 
 
   # Enable/disable caching. By default caching is disabled.
